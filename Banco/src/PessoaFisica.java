0117bdb25ce88e3c6cd7224c2bba1d56725ca6b0
joaohelbel <joao.helbel@acad.espm.br>
2024-10-03 15:23:58 -0300

    Commit PF e PJ

--- Banco/src/PessoaFisica.java
+++ Banco/src/PessoaFisica.java
@@ -2,5 +2,26 @@
 // A Classe PessoaFisica extenderá a classe Cliente e terá seus atributos + CPF
 
 public class PessoaFisica extends Cliente {
+    private final String cpf;
+
+    public PessoaFisica(String cpf){
+        super();
+        this.cpf = cpf;
+    }
+
+    public PessoaFisica(String nome, String cpf){
+        super(nome);
+        this.cpf = cpf;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\nCPF: " + cpf;
+    }
+
     
 }
